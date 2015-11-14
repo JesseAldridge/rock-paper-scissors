53885aee9aef176f94304cf4d3e507bd19bf1ea0
Jesse Aldridge <JesseAldridge@gmail.com>
2015-11-13 23:17:51 -0800

    don't show their choice until made mine

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -1,29 +1,47 @@
 Players = new Mongo.Collection("players");
 
 if (Meteor.isClient) {
+  var get_player_id = function() {
+    var match = /player1|player2/.exec(window.location.href);
+    return match && match[0];
+  };
+
+  var find_me = function() {
+    console.log('me:', Players.findOne({name: get_player_id()}));
+    return Players.findOne({name: get_player_id()});
+  };
+
+  var find_them = function() {
+    var player_id = get_player_id();
+    return (
+      player_id &&
+      Players.findOne({name: player_id == 'player1' ? 'player2' : 'player1'}));
+  };
+
   Template.body.helpers({
-    players: function () {
+    players: function() {
       return Players.find({});
+    },
+    player_id: function() {
+      return get_player_id();
+    },
+    me: function() {
+      return find_me();
+    },
+    them: function() {
+      return find_them();
+    },
+    score: function(player_id) {
+      var player = Players.findOne({name: player_id});
+      console.log('  res:', player && player.score);
+      return player && player.score;
     }
   });
 
-  Template.player.helpers({
-    is_me: function() {
-      return this.name == 'player1';
-    }
-  });
-
-  Template.player.events({
-    'click .rock': function(event) {
-      Players.update(this._id, { $set: {choice: 'rock'} });
-    },
-
-    'click .paper': function() {
-      Players.update(this._id, { $set: {choice: 'paper'} });
-    },
-
-    'click .scissors': function() {
-      Players.update(this._id, { $set: {choice: 'scissors'} });
+  Template.body.events({
+    'click div[choice]': function(event) {
+      console.log('choice:', $(event.target).attr('choice'));
+      Players.update(find_me()._id, { $set: {choice: $(event.target).attr('choice')} });
     }
   });
 }
@@ -44,8 +62,13 @@ if (Meteor.isServer) {
     else if(result == -1)
       player_b.score += 1;
 
-    Players.update({name:player_a.name}, {$set: {score: player_a.score, choice: null}});
-    Players.update({name:player_b.name}, {$set: {score: player_b.score, choice: null}});
+    Meteor.setTimeout(function() {
+      console.log('resolving round');
+      Players.update(
+        {name:player_a.name}, {$set: {score: player_a.score, choice: null}});
+      Players.update(
+        {name:player_b.name}, {$set: {score: player_b.score, choice: null}});
+    }, 1000);
   };
 
   var check_both_choices = function() {
