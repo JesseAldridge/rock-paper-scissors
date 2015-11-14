e68b5a161b20bc0e5badeddc9ac68492e5059ad2
Jesse Aldridge <JesseAldridge@gmail.com>
2015-11-13 23:26:00 -0800

    take out console.logs

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -33,24 +33,19 @@ if (Meteor.isClient) {
     },
     score: function(player_id) {
       var player = Players.findOne({name: player_id});
-      console.log('  res:', player && player.score);
       return player && player.score;
     }
   });
 
   Template.body.events({
     'click div[choice]': function(event) {
-      console.log('choice:', $(event.target).attr('choice'));
       Players.update(find_me()._id, { $set: {choice: $(event.target).attr('choice')} });
     }
   });
 }
 
 if (Meteor.isServer) {
-  var players = {};
-
   var process_choices = function(player_a, player_b) {
-    console.log('process_choices:', player_a.choice, player_b.choice);
     var result = {
       rock: {rock: 0, paper: -1, scissors: 1},
       paper: {rock: 1, paper: 0, scissors: -1},
@@ -63,7 +58,6 @@ if (Meteor.isServer) {
       player_b.score += 1;
 
     Meteor.setTimeout(function() {
-      console.log('resolving round');
       Players.update(
         {name:player_a.name}, {$set: {score: player_a.score, choice: null}});
       Players.update(
@@ -71,8 +65,9 @@ if (Meteor.isServer) {
     }, 1000);
   };
 
+  var players = {};
+
   var check_both_choices = function() {
-    console.log('check_both_choices, players:', Object.keys(players).length);
     if(Object.keys(players).length == 2 &&
        ['rock', 'paper', 'scissors'].indexOf(players.player1.choice) != -1 &&
        ['rock', 'paper', 'scissors'].indexOf(players.player2.choice) != -1)
@@ -81,16 +76,13 @@ if (Meteor.isServer) {
 
   Players.find({}).observe({
     added: function(player) {
-      console.log('added:', player);
       players[player.name] = player;
       check_both_choices();
     },
     removed: function(player) {
-      console.log('removed:', player);
       delete players[player.name];
     },
     changed: function(player, old_player) {
-      console.log('changed:', player);
       delete players[old_player.name];
       players[player.name] = player;
       check_both_choices();
